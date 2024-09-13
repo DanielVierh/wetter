a5885f609da097c02c4983affb6a1f5ebebf4dfb
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-09-13 20:04:28 +0200

    prevent empty search

--- app.js
+++ app.js
@@ -82,12 +82,16 @@ window.onload = loadData();
 //?####################################################################################################
 // Eingegebene Stadt suchen
 function showWeather() {
-    address = searchField.value;
-    getAddressCoordinates(address);
-    if (address.slice(-1) === ' ') {
-        address = address.trimEnd();
+    
+    if (searchField.value.length > 1) {
+        
+        address = searchField.value;
+        getAddressCoordinates(address);
+        if (address.slice(-1) === ' ') {
+            address = address.trimEnd();
+        }
+        searchField.value = '';
     }
-    searchField.value = '';
 }
 
 //?####################################################################################################
@@ -390,12 +394,12 @@ function requestWeatherForecast(lat, lon) {
             const humidity = data.current.humidity;
             let sultry = '';
 
-            if(temp > 20 && dewPoint > 16) {
+            if (temp > 20 && dewPoint > 16) {
                 sultry = 'Feuchtwarme Luft'
-            }else {
-                 sultry = ''
+            } else {
+                sultry = ''
             }
-            
+
             document.getElementById("outpHumidity").innerHTML = `${humidity}% <br/><br/> <hr> <span class="mini-headline">Taupunkt</span> <br/> <br/>${dewPoint}°C<br/><span class="sultry">${sultry}</span>`
 
 
@@ -515,7 +519,7 @@ function requestWeatherForecast(lat, lon) {
             for (let i = 0; i <= 24; i++) {
                 index = `hourForecastBlock${i}`;
                 document.getElementById(index).hidden = false;
-                
+
                 temp = parseInt(data.hourly[i].temp);
                 weatherIcon = data.hourly[i].weather[0].icon;
                 // ? Sommerzeit wird rausgerechnet
@@ -737,17 +741,17 @@ function get_RainData(data) {
     try {
         tomorrow_dewPoint = data.daily[1].dew_point.toFixed(0);
         tomorrow_temp = data.daily[1].temp.eve.toFixed(0);
-        if(tomorrow_dewPoint > 16 && tomorrow_temp > 20) {
+        if (tomorrow_dewPoint > 16 && tomorrow_temp > 20) {
             outputDewPoint.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
                     <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>
                   </svg>  Morgen liegt der Taupunkt bei ${tomorrow_dewPoint}°C. </br> Die Luft wird morgen feucht warm sein (Drückend).`
-        }else {
-             outputDewPoint.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
+        } else {
+            outputDewPoint.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
                     <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>
                   </svg>  Morgen liegt der Taupunkt bei ${tomorrow_dewPoint}°C.`
         }
     } catch (error) {
-        
+
     }
     try {
         rain = data.daily[0].rain;
@@ -1217,10 +1221,10 @@ function changeWeatherType(type) {
         }
         if (type === 'opt_dewPoint') {
             document.getElementById(index).innerHTML = `${nextDewPoint}°C`;
-            if(nextDewPoint > 16 && temp > 20) {
+            if (nextDewPoint > 16 && temp > 20) {
                 document.getElementById(index).style.color = 'orange'
-            }else {
-                 document.getElementById(index).style.color = 'white';
+            } else {
+                document.getElementById(index).style.color = 'white';
             }
             document.getElementById(index).classList.remove("active");
         }
@@ -1591,12 +1595,12 @@ days.forEach((day, index) => {
         const forc_dewPointVal = savedData.daily[index + 1].dew_point.toFixed(0);
         let forc_dewPoint = '';
         console.log('forc_dewPoint', forc_dewPoint);
-        if(forc_dewPointVal > 17) {
+        if (forc_dewPointVal > 17) {
             forc_dewPoint = `Taupunkt: ${forc_dewPointVal}°C (feuchtwarme Luft)`
-        }else {
+        } else {
             forc_dewPoint = `Taupunkt: ${forc_dewPointVal}°C`
         }
-        
+
         let content = `
             <p>${forc_desc}</p>
             <p>Regen: ${forc_rain} mm</p>
